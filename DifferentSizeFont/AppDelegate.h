349d73dd482b9b3938fa21286e9a9555de03d711
Marcos A Fonseca <marcosfonseca@Build-Mac-mini.local>
2016-12-28 16:48:50 -0200

    study day

---
+++ DifferentSizeFont/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DifferentSizeFont
+//
+//  Created by Marcos A Fonseca on 12/28/16.
+//  Copyright © 2016 Hildequias Junior. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DifferentSizeFont/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DifferentSizeFont
+//
+//  Created by Marcos A Fonseca on 12/28/16.
+//  Copyright © 2016 Hildequias Junior. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
